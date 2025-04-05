e9cace6c6c7eebaf76c2138920f74fd7624c441c
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-04-06 11:14:29 +1200

    add test database

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 
-import styles from './app.module.css'
+import styles from './app.module.css';
+import {Routes, Route} from 'react-router-dom';
+
+const Header = () => <div>Шапка</div>;
+const Footer = () => <div>Футер</div>;
 
 function App() {
 
   return (
     <>
-      <div className={styles.app}>
-
+      {/* <Header />
+      <div >
+      <i className="fa fa-user-o" aria-hidden="true"></i>
+    
+        <Routes>
+          <Route path="/" element={<div>Главная</div>} />
+          <Route path="/login" element={<div>Авторизация</div>} />
+          <Route path="/register" element={<div>Регистрация</div>} />
+          <Route path="/users" element={<div>Пользователи</div>} />
+          <Route path="/post" element={<div>Новая статья</div>} />
+          <Route path="/post/:postId" element={<div>Статья</div>} />
+          <Route path="*" element={<div>Ошибка</div>} />
+        </Routes>
       </div>
+      <Footer /> */}
+
     </>
   )
 }
