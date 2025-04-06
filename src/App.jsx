5b7a9829c8e127c3f78b825756d76618da37b540
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-04-06 12:09:32 +1200

    add routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 
 import styles from './app.module.css';
-import {Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-const Header = () => <div>Шапка</div>;
-const Footer = () => <div>Футер</div>;
+const Sidebar = () => <div>Шапка</div>;
+const Content = ({children}) => <div>{children}</div>;
 
 function App() {
 
   return (
     <>
-      {/* <Header />
-      <div >
-      <i className="fa fa-user-o" aria-hidden="true"></i>
-    
+      <Sidebar />
+      <Content>
         <Routes>
           <Route path="/" element={<div>Главная</div>} />
           <Route path="/login" element={<div>Авторизация</div>} />
           <Route path="/register" element={<div>Регистрация</div>} />
           <Route path="/users" element={<div>Пользователи</div>} />
-          <Route path="/post" element={<div>Новая статья</div>} />
-          <Route path="/post/:postId" element={<div>Статья</div>} />
+          <Route path="/courses" element={<div>Мои курсы</div>} />
+          <Route path="/courses/:courseSlug" element={<div>Страница курса</div>} />
+          <Route path="/courses/:courseSlug/:moduleSlug" element={<div>Страница модуля</div>} />
+          <Route path="/courses/:courseSlug/:moduleSlug/:lessonId" element={<div>Страница урока</div>} />
           <Route path="*" element={<div>Ошибка</div>} />
         </Routes>
-      </div>
-      <Footer /> */}
-
+      </Content>
     </>
   )
 }
