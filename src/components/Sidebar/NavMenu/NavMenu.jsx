179b27c02948bc0ace10a4e784d6490dfdf71ea2
MariaLight <61028743+MariaLight@users.noreply.github.com>
2025-05-14 01:06:28 +1200

    add user roles division in menu

--- src/components/Sidebar/NavMenu/NavMenu.jsx
+++ src/components/Sidebar/NavMenu/NavMenu.jsx
@@ -5,7 +5,7 @@ import { MenuItem } from '../MenuItem/MenuItem';
 export const NavMenu = ({ userRoleId }) => {
     return (
         <nav className={styles.header__nav}>
-            {/* {userRoleId === USER_ROLE_ID.admin &&
+            {userRoleId === USER_ROLE_ID.admin &&
                 <MenuItem icon="users" text="Пользователи" redirectTo="/users" />
 
             }
@@ -20,12 +20,12 @@ export const NavMenu = ({ userRoleId }) => {
             }
             {userRoleId === USER_ROLE_ID.student &&
                 <MenuItem icon="heart" text="Поддержка" redirectTo="/support" />
-            } */}
+            }
 
-            <MenuItem icon="users" text="Пользователи" redirectTo="/users" />
+            {/* <MenuItem icon="users" text="Пользователи" redirectTo="/users" />
             <MenuItem icon="book" text="Обучение" redirectTo="/courses" />
             <MenuItem icon="th-list" text="Каталог" redirectTo="/catalog" />
-            <MenuItem icon="heart" text="Поддержка" redirectTo="/support" />
+            <MenuItem icon="heart" text="Поддержка" redirectTo="/support" /> */}
         </nav>
     )
 }
